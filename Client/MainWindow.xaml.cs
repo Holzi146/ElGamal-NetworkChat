743c09de7f6d7aaedf3ff381ee66070181610d44
Holzi146 <holzhauser.christoph@gmx.at>
2014-02-24 18:37:45 +0100

    Initial commit

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+using System.Xml.Serialization;
+using System.IO;
+using System.Numerics;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Socket s;
+        Random zufall = new Random();
+        KeyStruct publicKey, cipher;
+        byte[] buffer = new byte[1024];
+
+        private void btn_connect_Click(object sender, RoutedEventArgs e)
+        {
+            s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                s.BeginConnect(new IPEndPoint(IPAddress.Parse(tbx_ip.Text), 1234), new AsyncCallback(DoConnect), s);
+                btn_connect.IsEnabled = false;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ungültige IP-Adresse!");
+            }
+            tbx_ip.Text = "";          
+        }
+
+        private void DoConnect(IAsyncResult res)
+        {
+            try
+            {
+                s = (Socket)res.AsyncState;
+                s.EndConnect(res);
+                s.BeginReceive(buffer, 0, 1024, SocketFlags.None, new AsyncCallback(DoReceive), s);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Verbindung fehlgeschlagen!");
+            }           
+        }
+
+        private void DoReceive(IAsyncResult res)
+        {
+            Socket c = (Socket)res.AsyncState;
+            c.EndReceive(res);
+            XmlSerializer xml_ser = new XmlSerializer(typeof(KeyStruct));
+            MemoryStream str = new MemoryStream(buffer);
+            /* Objekt deserialisieren und daraus den publicKey holen */
+            publicKey = (KeyStruct)xml_ser.Deserialize(str);
+            str.Close();
+
+            /* Erzeugung des großen B's --> g mit einem zufälligem Exponenten (kleines b) hochrechnen */
+            buffer = new byte[sizeof(UInt64)];
+            zufall.NextBytes(buffer);            
+            publicKey.b = BitConverter.ToUInt64(buffer, 0) % publicKey.p;
+            cipher = new KeyStruct {  B = (ulong)BigInteger.ModPow(publicKey.g, publicKey.b, publicKey.p)  };
+
+            Dispatcher.Invoke((Action)delegate 
+            {
+                img_check.Visibility = Visibility.Visible;
+                lbl_keyReceived.Visibility = Visibility.Visible;
+                btn_send.IsEnabled = true;
+            });           
+        }
+
+        private void btn_send_Click(object sender, RoutedEventArgs e)
+        {
+            XmlSerializer xml_ser = new XmlSerializer(typeof(KeyStruct));
+            MemoryStream str = new MemoryStream();
+            if (tbx_input.Text.Length > 71)
+            {
+                MessageBox.Show("Es sind nur maximal 71 Zeichen erlaubt!");
+                tbx_input.Text = "";
+                tbx_input.Focus();
+                return;
+            }
+            /* Ciphertext aus der Textbox und dem publicKey generieren */
+            cipher.c = ElGamalEncrypt(tbx_input.Text, publicKey);
+            /* Objekt serialisieren */
+            xml_ser.Serialize(str, cipher);
+            buffer = str.ToArray();
+            str.Close();
+
+            try
+            {
+                s.BeginSend(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(DoSend), s);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Server ist down!");
+            }        
+   
+            tbx_input.Text = "";
+            tbx_input.Focus();
+        }
+
+        private void DoSend(IAsyncResult res)
+        {
+            s.EndSend(res);
+        }
+
+        private ulong[] ElGamalEncrypt(string input, KeyStruct publicKey)
+        {
+            ulong[] c = new ulong[input.Length];
+
+            /* jedes Zeichen wird extra verschlüsselt und dann Element eines ulong[]'s */
+            for (int i = 0; i < input.Length; i++)
+                c[i] = (ulong)((BigInteger.ModPow(publicKey.A, publicKey.b, publicKey.p) * (int)input.ElementAt(i)) % publicKey.p);
+            return c;
+        }
+
+        private void tbx_input_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int count = 71 - tbx_input.Text.Length;
+            if (count < 0) count = 0;
+            lbl_zeichen.Content = "Geben Sie einen Text ein : (noch " + count.ToString() + " Zeichen übrig)";
+        }
+    }
+
+    [Serializable]
+    public class KeyStruct
+    {
+        public ulong p { get; set; }
+        public ulong g { get; set; }
+        public ulong a { get; set; }
+        public ulong A { get; set; }
+        public ulong B { get; set; }
+        public ulong b { get; set; }
+        public ulong[] c { get; set; }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Numerics;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace Server
+{
+    class Program
+    {
+        static Socket s, c;
+        static IPEndPoint endp, ipendp;
+        static Random zufall = new Random();
+        static KeyStruct publicKey, privateKey;
+        static byte[] buffer = new byte[1024];
+        
+        static void Main(string[] args)
+        {
+            Console.Title = "ElGamal Chat";
+            Console.WriteLine("Welcome to the ElGamal Chat System!\n");
+
+            KeyScheduling(ref publicKey, ref privateKey);
+
+            Console.WriteLine("Waiting for connections...\n");
+
+            s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            endp = new IPEndPoint(IPAddress.Any, 1234);
+            s.Bind(endp);         
+            s.Listen(3);
+            s.BeginAccept(new AsyncCallback(DoAccept), s);
+
+            Console.ReadLine();
+        }
+
+        private static void KeyScheduling(ref KeyStruct publicKey, ref KeyStruct privateKey)
+        {
+            byte[] buffer = new byte[sizeof(UInt64)];
+            /* q brauchen wir für das Erzeugen einer Sophie-Germain-Primzahl */
+            ulong q;
+
+            /* zuerst wird das q bestimmt, um so ein passendes p zu finden, *
+             * dies bringt den Vorteil, dass durch Sophie-Germain Zahlen die Primfaktorzerlegung für das g wegfällt */
+            do
+            {
+                zufall.NextBytes(buffer);
+                q = BitConverter.ToUInt64(buffer, 0);
+                /* durch das Shiften um eine Stelle wird erreicht, dass p durch die Multiplikation mit 2 und q nicht seinen Wertebereich überschreitet */
+                q >>= 1;
+            }
+            while (!MillerRabin(q) || !SophieGermain(q));
+
+            ulong p = (q << 1) + 1;
+            Console.WriteLine("p = " + p);
+
+            /* Bestimmung des Generatorpolynoms --> ist jetzt relativ einfach, da die beiden primen Teiler von phi(p) 2 und q sind */
+            ulong g = GetGenerator(p, q);
+            Console.WriteLine("g = " + g);
+
+            /* ein zufälliges geheimes a wird gewählt --> wichtig für den Private Key */
+            zufall.NextBytes(buffer);
+            ulong a = BitConverter.ToUInt64(buffer, 0) % p;
+            Console.WriteLine("a = " + a);
+
+            /* das große (öffentliche) A wird errechnet, mit dessen Hilfe Nachrichten verschlüsselt werden können */
+            ulong A = (ulong) BigInteger.ModPow(g, a, p);
+            Console.WriteLine("A = " + A + "\n");
+
+            publicKey = new KeyStruct { p = p, g = g, A = A };
+            privateKey = new KeyStruct { a = a };
+        }
+
+        private static bool SophieGermain(ulong zahl)
+        {
+            if (MillerRabin((zahl << 1) + 1))
+                return true;
+            return false;
+        }
+
+        private static ulong GetGenerator(ulong p, ulong q)
+        {
+            ulong g;         
+            byte[] buffer = new byte[sizeof(UInt64)];
+
+            while (true)
+            {
+                zufall.NextBytes(buffer);
+                /* um zu garantieren, dass g in der Menge von p ist, wird % p gerechnet */
+                g = BitConverter.ToUInt64(buffer, 0) % p;
+                /* dadurch, dass wir Sophie-Germain Zahlen als p verwenden (sprich die primen Teiler von phi(p) 2 und q sind,s
+                 * müssen bei der Überprüfung eines Generatorpolynoms nur diese zwei Zahlen herangezogen werden */
+                if(BigInteger.ModPow(g,q,p) != 1 && BigInteger.ModPow(g,2,p) != 1)
+                    return g;
+            }
+        }
+
+        /* Primzahltest nach dem MillerRabin-Verfahren */
+        private static bool MillerRabin(BigInteger zahl)
+        {
+            /* wenn die Zahl durch zwei teilbar ist */
+            if ((zahl & 1) == 0)
+                return false;
+
+            BigInteger d = zahl - 1;
+            int s = 0;
+
+            while ((d & 1) == 0)  {  d >>= 1; s++;  }
+
+            /* nach vier Versuchen hat man bereits eine Wahrscheinlichkeit von unter 0,04% */
+            for (int a = 2; a < 6; a++)
+            {
+                BigInteger x = BigInteger.ModPow(a, d, zahl);
+                if (x == 1 || x == zahl - 1)
+                    continue;
+
+                for (int r = 1; r < s; r++)
+                {
+                    x = BigInteger.ModPow(x, 2, zahl);
+                    if (x == 1)
+                        return false;
+                    if (x == zahl - 1)
+                        break;
+                }
+
+                if (x != zahl - 1)
+                    return false;
+            }
+            return true;
+        }
+
+        private static void DoAccept(IAsyncResult res)
+        {
+            s = (Socket)res.AsyncState;
+            c = s.EndAccept(res);
+            s.BeginAccept(new AsyncCallback(DoAccept), s);
+            SocketObject sObject = new SocketObject { clientSocket = c };
+
+            ipendp = (IPEndPoint)c.RemoteEndPoint;
+            Console.WriteLine(ipendp.Address + " --CONNECT--");
+
+            /* öffentlicher Schlüssel wird serialisiert und verschickt */
+            XmlSerializer xml_ser = new XmlSerializer(typeof(KeyStruct));
+            MemoryStream str = new MemoryStream();
+            xml_ser.Serialize(str, publicKey);
+            buffer = str.ToArray();
+            c.BeginSend(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(DoSend), c);
+            str.Close();
+
+            /* Nachrichten werden empfangen */
+            sObject.buffer = new byte[4096];
+            c.BeginReceive(sObject.buffer, 0, 4096, SocketFlags.None, new AsyncCallback(DoReceive), sObject);
+        }
+
+        private static void DoReceive(IAsyncResult res)
+        {
+            try
+            {
+                SocketObject sObject = (SocketObject)res.AsyncState;
+                sObject.clientSocket.EndReceive(res);
+
+                XmlSerializer xml_ser = new XmlSerializer(typeof(KeyStruct));
+                MemoryStream str = new MemoryStream(sObject.buffer);
+                /* Objekt deserialisieren und entschlüsseln */
+                Console.WriteLine(ipendp.Address + " " + ElGamalDecrypt((KeyStruct)xml_ser.Deserialize(str), privateKey));
+                str.Close();
+
+                /* Wiederholen des Receive-Vorgangs */
+                sObject.buffer = new byte[4096];
+                sObject.clientSocket.BeginReceive(sObject.buffer, 0, 4096, SocketFlags.None, new AsyncCallback(DoReceive), sObject);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(ipendp.Address + " --DISCONNECT--\n");
+            }          
+        }
+
+        private static void DoSend(IAsyncResult res)
+        {
+            c.EndSend(res);
+        }
+
+        private static string ElGamalDecrypt(KeyStruct cipher, KeyStruct privateKey)
+        {
+            string m = "";
+            ulong x = publicKey.p - 1 - privateKey.a;
+
+            /* jedes Zeichen wird wieder entschlüsselt */
+            for (int i = 0; i < cipher.c.Length; i++)
+                m += char.ConvertFromUtf32((int)((BigInteger.ModPow(cipher.B, x, publicKey.p) * cipher.c[i]) % publicKey.p));          
+            return m;
+        }
+    }
+
+    [Serializable]
+    public class KeyStruct
+    {
+        public ulong p { get; set; }
+        public ulong g { get; set; }
+        public ulong a { get; set; }
+        public ulong A { get; set; }
+        public ulong B { get; set; }
+        public ulong b { get; set; }
+        public ulong [] c { get; set; }
+    }
+
+    public class SocketObject
+    {
+        public byte[] buffer = new byte[4096];
+        public Socket clientSocket;
+    }
+}
